4bd40ab01538c37fa0767e60200e9209595830a4
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-07-11 18:18:49 +0200

    added contacts and scroll reveal

---
+++ js/scroll-reveal.js
@@ -0,0 +1,7 @@
+let revealOptions = {};
+revealOptions.container = ".wrapper";
+revealOptions.duration = 1000;
+revealOptions.reset = true;
+revealOptions.delay = 150;
+
+ScrollReveal().reveal('section', revealOptions);

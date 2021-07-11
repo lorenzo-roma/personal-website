6b847bdc4b6ca65a0a6eb346ea4ae7638f485a66
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-07-11 23:30:20 +0200

    removing bar when first page

--- js/progress.js
+++ js/progress.js
@@ -1,12 +1,18 @@
 // When the user scrolls the page, execute myFunction
 const scrollable = document.getElementsByClassName("wrapper")[0];
+const progressBar =  document.getElementById("myBar");
+const emptyProgressBar = document.getElementById("empty-bar");
 scrollable.onscroll = function() {myFunction()};
 
 function myFunction() {
-  const winScroll = scrollable.scrollTop;
-  const height = scrollable.scrollHeight - scrollable.clientHeight;
-  const scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style['mask-size'] = scrolled + "% 100%";
-  document.getElementById("myBar").style['-webkit-mask-size'] = scrolled + "% 100%";
+  let winScroll = scrollable.scrollTop;
+  let height = scrollable.scrollHeight - scrollable.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  let hasToBeVisible = (scrolled > 10) ? 1 : 0;
+  progressBar.style.opacity = hasToBeVisible;
+  emptyProgressBar.style.opacity = hasToBeVisible;
+  progressBar.style['mask-size'] = scrolled + "% 100%";
+  progressBar.style['-webkit-mask-size'] = scrolled + "% 100%";
+
 
 }
